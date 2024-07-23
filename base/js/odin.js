fc0d3ff9c2d30ab8ab2fbff9f7099448c7ad689e
Logan Swanson <logswan9@gmail.com>
2024-07-22 23:04:13 -0500

    Added basic ui for rps

--- base/js/odin.js
+++ base/js/odin.js
@@ -1,50 +1,105 @@
 //const choice = prompt("Enter your choice");
 
-var compChoice = Math.floor(Math.random() * 3);
+var compChoice;
+var choice;
+var userScore = 0;
+var compScore = 0;
 
-const choice = prompt("Enter your choice");
+const rockButton = document.getElementById('rockBtn');
+const paperButton = document.getElementById('paperBtn');
+const scissorsButton = document.getElementById('scissorsBtn');
+const resetButton = document.getElementById('resetScore');
 
-switch (compChoice) {
-    case 0:
-        compChoice = "Rock";
-    break;
-    case 1:
-        compChoice = "Paper";
-    break;
-    case 2:
-        
-        compChoice = "Scissors";
-    break;
-    default:
+
+rockButton.addEventListener("click", function() {
+    document.getElementById('userChoice').innerHTML = "You chose Rock!"
+    compChoice = Math.floor(Math.random() * 3);
+    choice = "r";
+    checkWinner(choice, compChoice);
+});
+
+paperButton.addEventListener("click", function() {
+    document.getElementById('userChoice').innerHTML = "You chose Paper!"
+    compChoice = Math.floor(Math.random() * 3);
+    choice = "p";
+    checkWinner(choice, compChoice);
+});
+
+scissorsButton.addEventListener("click", function() {
+    document.getElementById('userChoice').innerHTML = "You chose Scissors!"
+    compChoice = Math.floor(Math.random() * 3);
+    choice = "s";
+    checkWinner(choice, compChoice);
+});
+
+resetButton.addEventListener("click", function() {
+    userScore = 0;
+    compScore = 0;
+    document.getElementById('resetScore').style.display = 'none';
+    document.getElementById('score').innerHTML = "You: " + userScore + " Comp: " + compScore;
+});
+
+//const choice = prompt("Enter your choice");
+function checkWinner(choice, compChoice) {
+    switch (compChoice) {
+        case 0:
+            compChoice = "Rock";
         break;
+        case 1:
+            compChoice = "Paper";
+        break;
+        case 2:
+            
+            compChoice = "Scissors";
+        break;
+        default:
+            break;
+    }
+    
+    
+    
+    
+    
+
+    if (choice == "r") {
+        if (compChoice == 'Scissors') {
+            document.getElementById('results').innerHTML = "Computer went with Scissors.\nYou Win!"
+            userScore++;
+        } else if (compChoice == 'Rock') {
+            document.getElementById('results').innerHTML = "Computer went with Rock as well.\nYou tie!"
+        } else {
+            document.getElementById('results').innerHTML = "Computer went with Paper.\nYou lose :("
+            compScore++;
+        }
+        document.getElementById('score').innerHTML = "You: " + userScore + " Comp: " + compScore;
+    }
+    if (choice == "p") {
+        if (compChoice == 'Rock') {
+            document.getElementById('results').innerHTML = "Computer went with Rock.\nYou Win!"
+            userScore++;
+        } else if (compChoice == 'Paper') {
+            document.getElementById('results').innerHTML = "Computer went with Paper as well.\nYou tie!"
+        } else {
+            document.getElementById('results').innerHTML = "Computer went with Scissors.\nYou lose :("
+            compScore++;
+        }
+        document.getElementById('score').innerHTML = "You: " + userScore + " Comp: " + compScore;
+    }
+    if (choice == "s") {
+        if (compChoice == 'Paper') {
+            document.getElementById('results').innerHTML = "Computer went with Paper.\nYou Win!"
+            userScore++;
+        } else if (compChoice == 'Scissors') {
+            document.getElementById('results').innerHTML = "Computer went with Scissors as well.\nYou tie!"
+        } else {
+            document.getElementById('results').innerHTML = "Computer went with Rock.\nYou lose :("
+            compScore++;
+        }
+        document.getElementById('score').innerHTML = "You: " + userScore + " Comp: " + compScore;
+    }
+
+    document.getElementById('resetScore').style.display = 'block';
+    
 }
 
-if (choice == "r") {
-    if (compChoice == 'Scissors') {
-        console.log("You Win!");
-    } else if (compChoice == 'Rock') {
-        console.log("You tie!");
-    } else {
-        console.log("You lose!");
-    }
-}
-if (choice == "p") {
-    if (compChoice == 'Rock') {
-        console.log("You Win!");
-    } else if (compChoice == 'Paper') {
-        console.log("You tie!");
-    } else {
-        console.log("You lose!");
-    }
-}
-if (choice == "s") {
-    if (compChoice == 'Paper') {
-        console.log("You Win!");
-    } else if (compChoice == 'Scissors') {
-        console.log("You tie!");
-    } else {
-        console.log("You lose!");
-    }
-}
 
-console.log(compChoice);
